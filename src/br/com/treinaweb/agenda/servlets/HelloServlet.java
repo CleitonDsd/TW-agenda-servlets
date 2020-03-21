bcb55e13049fe4b4080340bb233d5a3df5c41d97
Cleiton Dias <s.diascleiton@gmail.com>
2020-03-21 12:32:15 -0300

    Implementando o servlet, populando informações

--- src/br/com/treinaweb/agenda/servlets/HelloServlet.java
+++ src/br/com/treinaweb/agenda/servlets/HelloServlet.java
@@ -1,6 +1,7 @@
 package br.com.treinaweb.agenda.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,10 +10,33 @@ import javax.servlet.http.HttpServletResponse;
 
 public class HelloServlet extends HttpServlet {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
-	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.service(req, resp);
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// uma maneira de acessarmos o response e escrevermos um resultado a ele
+		// parecido com sys.out.print
+
+		PrintWriter out = response.getWriter();
+
+		// como browser só aceitam HTML,CSS e JS, vamos devolver a response em HTML para
+		// o browser
+
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Olá, a partir do Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Olá, mundo!</h1>");
+		out.println("<p>Eu sou uma response, vim do Servlet</p>");
+		out.println("</body>");
+		out.println("</html>");
+
 	}
 
 }
